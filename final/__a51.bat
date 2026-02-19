083728afbc42bdfc64877f576971267fb545ff5e
gracech2025 <cgracecac@gmail.com>
2026-02-19 11:33:06 -0800

    FINAL COEDEEE

---
+++ final/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\ti\Documents\Reflow-Controller-ELEC291-main\final\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\ti\Documents\Reflow-Controller-ELEC291-main\final\keypad_test0218.asm"
+echo Crosside_Action Set_Hex_File C:\Users\ti\Documents\Reflow-Controller-ELEC291-main\final\keypad_test0218.HEX
